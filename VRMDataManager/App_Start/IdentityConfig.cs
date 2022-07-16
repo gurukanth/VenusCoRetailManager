b91f31fdc9c7994f321713ab7ef7f470eb90e97d
Venugopal Dasari <venugrda@gmail.com>
2022-07-16 13:14:30 +0530

    updated password length configuration

--- VRMDataManager/App_Start/IdentityConfig.cs
+++ VRMDataManager/App_Start/IdentityConfig.cs
@@ -28,11 +28,11 @@ namespace VRMDataManager
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequiredLength = 8,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
-                RequireUppercase = true,
+                RequireUppercase = false,
             };
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
