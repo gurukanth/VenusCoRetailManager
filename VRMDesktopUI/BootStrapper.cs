092f7552b0a2cc41c5ef650b5534f0df6a1e86c7
Venugopal Dasari <venugrda@gmail.com>
2022-07-16 16:54:38 +0530

    Added Data Project and Desktop View Project
    Updated desktop view to calibrn mvvm view

---
+++ VRMDesktopUI/BootStrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using VRMDesktopUI.ViewModels;
+
+namespace VRMDesktopUI
+{
+    class BootStrapper : BootstrapperBase
+    {
+        public BootStrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
