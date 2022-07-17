cfc0bdaf090c258f840d0cf14f245af4a33ea1c3
Venugopalarao Dasari <vdasari20@dxc.com>
2022-07-17 20:56:15 +0530

    Added tables to the VRMdata for Cash Register

---
+++ VRMData/dbo/Tables/Sale.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Sale]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[CashierId] NVARCHAR(128) NOT NULL,
+	[SaleDate] DATETIME2 NOT NULL,
+	[SubTotal] money NOT NULL,
+	[Tax] money NOT NULL,
+	[Total] money NOT NULL
+)
---
+++ VRMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[SaleId] INT NOT NULL,
+	[ProductId] INT NOT NULL,
+	[Quantity] INT NOT NULL DEFAULT 1,
+	[PurchasePrice] money NOT NULL,
+	[Tax] money DEFAULT 0,
+)
